6804e19aa059e9cdf0d35caedd03f49053132067
Paul Aglipay <PAglipay@ad.it.ucla.edu>
2022-02-07 12:02:59 -0800

    Added more tests to ArrayDeque2

--- custom-collections/src/main/java/com/revature/custom_collections/CollectionDriver.java
+++ custom-collections/src/main/java/com/revature/custom_collections/CollectionDriver.java
@@ -37,13 +37,39 @@ public class CollectionDriver {
         hs.add("data");
         System.out.println(hs.isEmpty());
     	
-    	// ArrayDeque adq = new ArrayDeque();
-    	
-    	// System.out.println(adq.peekFirst());
-    	
-    	// adq.add(27);
-    	
-    	// System.out.println(adq.peekLast());
+        System.out.println("ArrayDeque2: ---------------");
+        ArrayDeque<String> int_deque = new ArrayDeque<>();
+		  
+        int_deque.add("Hello1"); int_deque.add("Hello2"); int_deque.add("Hello3");
+        int_deque.addLast("Hello4"); int_deque.addLast("Hello5");
+        
+        System.out.println("added 5 hellos");
+        
+        System.out.println(int_deque.size()); 
+        System.out.println(int_deque.peek());
+        
+        System.out.println("removing first"); 
+        System.out.println(int_deque.poll());
+        System.out.println(int_deque.size()); 
+        System.out.println(int_deque.peek());
+        
+        System.out.println("removing first");
+        System.out.println(int_deque.pollFirst());
+        System.out.println(int_deque.size()); 
+        System.out.println(int_deque.peek());
+        
+        System.out.println("removing last");
+        System.out.println(int_deque.pollLast());
+        System.out.println(int_deque.isEmpty());
+        
+        System.out.println(int_deque.peekLast()); 
+        int_deque.addLast("Hello6");
+        System.out.println(int_deque.peekLast());
+        
+        System.out.println(int_deque.pollFirst());
+        System.out.println(int_deque.pollLast());
+        System.out.println(int_deque.size());
+        System.out.println(int_deque.isEmpty());
     	
     }
 }
