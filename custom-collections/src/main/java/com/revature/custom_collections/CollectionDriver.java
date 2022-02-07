b354150aa2c7636bd008fd6a144948d0a422f172
Austin Blandford <ablandford2399@gmail.com>
2022-02-07 12:06:47 -0800

    commented collection driver.

--- custom-collections/src/main/java/com/revature/custom_collections/CollectionDriver.java
+++ custom-collections/src/main/java/com/revature/custom_collections/CollectionDriver.java
@@ -6,63 +6,63 @@ public class CollectionDriver {
 
 	public static void main(String[] args) {
 
-		ArrayDeque<Integer> adq = new ArrayDeque<Integer>(2);
-
-		System.out.println("Is the arrayDeque empty: " + adq.isEmpty());
-
-		System.out.println("Element was added: " + adq.add(1));
-
-		System.out.println("Is the arrayDeque empty: " + adq.isEmpty());
-
-		System.out.println("Size of ArrayDeque: " + adq.size());
-
-		System.out.println(adq.peekLast());
-
-		System.out.println(adq.peekFirst());
-
-		System.out.println("Element was added: " + adq.add(2));
-
-		System.out.println("Size of ArrayDeque: " + adq.size());
-
-		System.out.println(adq.peekLast());
-
-		System.out.println(adq.peekFirst());
-
-		System.out.println("Element was added: " + adq.add(4));
-
-		System.out.println("ArrayDeque contains 2: " + adq.contains(2));
-
-		System.out.println("Size of ArrayDeque: " + adq.size());
-
-		System.out.println(adq.peekLast());
-
-		System.out.println(adq.peekFirst());
-
-		adq.addFirst(18);
-
-		System.out.println("ArrayDeque contains 1: " + adq.contains(1));
-
-		System.out.println("Size of ArrayDeque: " + adq.size());
-
-		System.out.println(adq.peekLast());
-
-		System.out.println(adq.peekFirst());
-
-		adq.addLast(27);
-
-		System.out.println("ArrayDeque contains 4: " + adq.contains(4));
-
-		System.out.println("Size of ArrayDeque: " + adq.size());
-
-		System.out.println(adq.peekLast());
-
-		System.out.println(adq.peekFirst());
-		
-		System.out.println("Remove element 1: " + adq.remove(1));
-		
-		System.out.println("ArrayDeque contains 1: " + adq.contains(1));
-		
-		System.out.println("Size of ArrayDeque: " + adq.size());
+//		ArrayDeque<Integer> adq = new ArrayDeque<Integer>(2);
+//
+//		System.out.println("Is the arrayDeque empty: " + adq.isEmpty());
+//
+//		System.out.println("Element was added: " + adq.add(1));
+//
+//		System.out.println("Is the arrayDeque empty: " + adq.isEmpty());
+//
+//		System.out.println("Size of ArrayDeque: " + adq.size());
+//
+//		System.out.println(adq.peekLast());
+//
+//		System.out.println(adq.peekFirst());
+//
+//		System.out.println("Element was added: " + adq.add(2));
+//
+//		System.out.println("Size of ArrayDeque: " + adq.size());
+//
+//		System.out.println(adq.peekLast());
+//
+//		System.out.println(adq.peekFirst());
+//
+//		System.out.println("Element was added: " + adq.add(4));
+//
+//		System.out.println("ArrayDeque contains 2: " + adq.contains(2));
+//
+//		System.out.println("Size of ArrayDeque: " + adq.size());
+//
+//		System.out.println(adq.peekLast());
+//
+//		System.out.println(adq.peekFirst());
+//
+//		adq.addFirst(18);
+//
+//		System.out.println("ArrayDeque contains 1: " + adq.contains(1));
+//
+//		System.out.println("Size of ArrayDeque: " + adq.size());
+//
+//		System.out.println(adq.peekLast());
+//
+//		System.out.println(adq.peekFirst());
+//
+//		adq.addLast(27);
+//
+//		System.out.println("ArrayDeque contains 4: " + adq.contains(4));
+//
+//		System.out.println("Size of ArrayDeque: " + adq.size());
+//
+//		System.out.println(adq.peekLast());
+//
+//		System.out.println(adq.peekFirst());
+//		
+//		System.out.println("Remove element 1: " + adq.remove(1));
+//		
+//		System.out.println("ArrayDeque contains 1: " + adq.contains(1));
+//		
+//		System.out.println("Size of ArrayDeque: " + adq.size());
 
 	}
 }
