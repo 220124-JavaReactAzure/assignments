9d3fed7ccff3f4e6c0ebc5a26eec8ae98a59c32d
JesterCharles <charles.jester@revature.com>
2022-02-15 15:00:57 -0500

    Implemented the director section of the hibernateChallenge

--- hiberanteChallenge/src/test/java/com/revature/helloHibernate/util/HibernateUtilTestSuite.java
+++ hiberanteChallenge/src/test/java/com/revature/helloHibernate/util/HibernateUtilTestSuite.java
@@ -1,6 +1,9 @@
 package com.revature.helloHibernate.util;
 
 
+import static org.junit.Assert.assertNotNull;
+
+import org.hibernate.Session;
 import org.junit.Test;
 
 
@@ -9,7 +12,14 @@ public class HibernateUtilTestSuite {
 	// TODO: Implement me like a connection factory test suite
 	@Test
 	public void test_getSession_returnsValidSessuib_givenProviderCredentials() {
-		
+		try {
+			Session sess = HibernateUtil.getSession();
+			HibernateUtil.closeSession();
+			System.out.println(sess);
+			assertNotNull(sess);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 }
